3e93464cf623c986516e868c5121e5ec7d1099e1
nurulislamrimon <nurulislamrimon@gmail.com>
2024-09-19 14:10:56 +0600

    initiate

---
+++ src/Editor.jsx
@@ -0,0 +1,157 @@
+import { useState, useRef, useEffect } from "react";
+import EditorUI from "./EditorUI";
+import { usePhotoUpload } from "./usePhotoUpload";
+
+function Editor() {
+  const editorRef = useRef(null);
+  const [content, setContent] = useState("");
+  const [images, setImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const { uploadPhotosAndUpdateContent, isUploading, uploadError } =
+    usePhotoUpload();
+
+  // prevent user from leaving page without saving
+  useEffect(() => {
+    window.addEventListener("beforeunload", function (event) {
+      event.preventDefault();
+      event.returnValue = "Are you sure you want to leave?";
+    });
+    // after clicking the leave button / final close
+    window.addEventListener("unload", function () {
+      // deleteUploadedFiles();
+      // Use navigator.sendBeacon to send the request when the page is closed
+      const url = "http://localhost:5000";
+      // const data = JSON.stringify({ message: "User closed the browser" });
+
+      // navigator.sendBeacon(url, data);
+      navigator.sendBeacon(url);
+    });
+  });
+
+  // set input
+  const handleSetInputInState = () => {
+    setContent(editorRef.current.innerHTML);
+  };
+
+  // handle focus editor
+  const focusEditor = () => {
+    editorRef.current.focus();
+  };
+
+  // handle toolbar clicked
+  const handleToolbarClick = (event) => {
+    const command = event.target.value;
+    if (command === "h1" || command === "h2" || command === "p") {
+      document.execCommand(
+        "formatBlock",
+        false,
+        command === "p" ? "p" : `<${command}>`
+      );
+    } else if (event.target.tagName === "BUTTON") {
+      document.execCommand(event.target.value.toLowerCase());
+      focusEditor();
+    }
+  };
+
+  // handle insert image in markup
+  const insertImageInMarkup = (imageUrl) => {
+    const editor = editorRef.current;
+
+    // Save the current selection and range
+    const selection = window.getSelection();
+
+    // Insert the image
+    document.execCommand("insertImage", false, imageUrl);
+
+    // Create a text node after the image to ensure you can type immediately after it
+    const image =
+      editor.getElementsByTagName("img")[
+        editor.getElementsByTagName("img").length - 1
+      ];
+
+    if (image) {
+      // Create a new text node and insert it after the image
+      const textNode = document.createTextNode("\u00A0"); // Non-breaking space
+      image.parentNode.insertBefore(textNode, image.nextSibling);
+
+      // Move cursor to the newly created text node
+      const newRange = document.createRange();
+      newRange.setStart(textNode, 0); // Set cursor at the start of the text node
+      newRange.collapse(true);
+      selection.removeAllRanges();
+      selection.addRange(newRange);
+    }
+
+    // Update content state
+    handleSetInputInState();
+  };
+
+  // handle upload image in the server and insert
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setImages((prev) => [...prev, file]);
+      const imageURL = URL.createObjectURL(file);
+      insertImageInMarkup(imageURL);
+    }
+  };
+
+  // handle clicked image to remove
+  const handleImageClick = (event) => {
+    if (event.target.tagName === "IMG") {
+      setSelectedImage(event.target);
+    } else {
+      setSelectedImage(null);
+    }
+  };
+
+  // handle remove image
+  const handleRemoveImage = () => {
+    if (selectedImage) {
+      selectedImage.remove();
+      setSelectedImage(null);
+      handleSetInputInState();
+    }
+  };
+
+  // handle key down to prevent image remove when pressing backspace
+  const handleKeyDown = (event) => {
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    if (event.key === "Backspace" && range) {
+      if (range.collapsed) {
+        const nodeBefore = range.startContainer.previousSibling;
+        if (nodeBefore && nodeBefore.tagName === "IMG") {
+          event.preventDefault();
+        }
+      }
+    }
+  };
+
+  // handle submit
+  const handleSubmit = async () => {
+    const data = await uploadPhotosAndUpdateContent({ content, images });
+    console.log(data);
+
+    // setContent(data);
+  };
+
+  return (
+    <EditorUI
+      content={content}
+      editorRef={editorRef}
+      handleToolbarClick={handleToolbarClick}
+      handleSetInputInState={handleSetInputInState}
+      handleImageUpload={handleImageUpload}
+      handleImageClick={handleImageClick}
+      handleRemoveImage={handleRemoveImage}
+      selectedImage={selectedImage}
+      handleKeyDown={handleKeyDown}
+      handleSubmit={handleSubmit}
+      isUploading={isUploading}
+      uploadError={uploadError}
+    />
+  );
+}
+
+export default Editor;
---
+++ src/EditorUI.jsx
@@ -0,0 +1,163 @@
+/* eslint-disable react/prop-types */
+import "./editor.css";
+
+function EditorUI({
+  editorRef,
+  handleToolbarClick,
+  handleSetInputInState,
+  handleImageUpload,
+  handleImageClick,
+  handleRemoveImage,
+  selectedImage,
+  handleKeyDown,
+  handleSubmit,
+  isUploading,
+  uploadError,
+}) {
+  return (
+    <div className="es--editor-container">
+      <div className="toolbar">
+        {/* bold italic and underline */}
+        <div>
+          <button
+            title="Bold"
+            onClick={handleToolbarClick}
+            value="bold"
+            aria-label="Bold"
+          >
+            B
+          </button>
+          <button
+            title="Italic"
+            onClick={handleToolbarClick}
+            value="italic"
+            aria-label="Italic"
+          >
+            I
+          </button>
+          <button
+            title="Underline"
+            onClick={handleToolbarClick}
+            value="underline"
+            aria-label="Underline"
+          >
+            U
+          </button>
+        </div>
+
+        {/* alignment */}
+        <div>
+          <button
+            title="Align Left"
+            onClick={handleToolbarClick}
+            value="justifyLeft"
+            aria-label="Align Left"
+            className="align align-left"
+          ></button>
+          <button
+            title="Align Center"
+            onClick={handleToolbarClick}
+            value="justifyCenter"
+            aria-label="Align Center"
+            className="align align-center"
+          >
+            {" "}
+          </button>
+          <button
+            title="Align Right"
+            onClick={handleToolbarClick}
+            value="justifyRight"
+            aria-label="Align Right"
+            className="align align-right"
+          ></button>
+        </div>
+
+        {/* tags */}
+        <div>
+          <button
+            title="Add a Title"
+            onClick={handleToolbarClick}
+            value="h1"
+            aria-label="Heading 1"
+          >
+            H1
+          </button>
+          <button
+            title="Add a Subtitle"
+            onClick={handleToolbarClick}
+            value="h2"
+            aria-label="Heading 2"
+          >
+            H2
+          </button>
+          <button
+            title="Add a Paragraph"
+            onClick={handleToolbarClick}
+            value="p"
+            aria-label="Paragraph"
+          >
+            P
+          </button>
+        </div>
+
+        {/* image upload and remove*/}
+        <div>
+          <button title="Upload an Image">
+            <label
+              htmlFor="photo"
+              style={{ fontSize: "26px", marginTop: "-3px", cursor: "pointer" }}
+            >
+              🖼️
+            </label>
+            <input
+              style={{ display: "none" }}
+              type="file"
+              id="photo"
+              onChange={handleImageUpload}
+              aria-label="Upload Image"
+            />
+          </button>
+          <button
+            title="Select an Image to remove!"
+            onClick={handleRemoveImage}
+            disabled={!selectedImage}
+            aria-label="Remove Image"
+          >
+            ❌
+          </button>
+        </div>
+      </div>
+      {isUploading && (
+        <p
+          style={{
+            position: "absolute",
+            inset: "0",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Uploading...
+        </p>
+      )}
+      {/* The contentEditable div where users type */}
+      <div
+        ref={editorRef}
+        onInput={handleSetInputInState}
+        onClick={handleImageClick}
+        onKeyDown={handleKeyDown}
+        contentEditable
+        className="editor-content"
+        style={
+          uploadError && {
+            border: "1px solid red",
+          }
+        }
+      />
+
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default EditorUI;
